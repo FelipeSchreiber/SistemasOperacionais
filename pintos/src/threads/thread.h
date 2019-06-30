70e33291ac265d5683800b6f239abb3ab358b97f
FelipeSchreiber <schreiber.felipe@poli.ufrj.br>
2019-06-30 16:14:22 -0300

    Update thread.h

--- pintos/src/threads/thread.h
+++ pintos/src/threads/thread.h
@@ -4,7 +4,7 @@
 #include <debug.h>
 #include <list.h>
 #include <stdint.h>
-#include "threads/synch.h"
+#include "fixed-point.h"
 #include "../devices/timer.h"
 
 /* States in a thread's life cycle. */
@@ -27,13 +27,11 @@ typedef int tid_t;
 #define PRI_MAX 63                      /* Highest priority. */
 
 /* A kernel thread or user process.
-
    Each thread structure is stored in its own 4 kB page.  The
    thread structure itself sits at the very bottom of the page
    (at offset 0).  The rest of the page is reserved for the
    thread's kernel stack, which grows downward from the top of
    the page (at offset 4 kB).  Here's an illustration:
-
         4 kB +---------------------------------+
              |          kernel stack           |
              |                |                |
@@ -55,22 +53,18 @@ typedef int tid_t;
              |               name              |
              |              status             |
         0 kB +---------------------------------+
-
    The upshot of this is twofold:
-
       1. First, `struct thread' must not be allowed to grow too
          big.  If it does, then there will not be enough room for
          the kernel stack.  Our base `struct thread' is only a
          few bytes in size.  It probably should stay well under 1
          kB.
-
       2. Second, kernel stacks must not be allowed to grow too
          large.  If a stack overflows, it will corrupt the thread
          state.  Thus, kernel functions should not allocate large
          structures or arrays as non-static local variables.  Use
          dynamic allocation with malloc() or palloc_get_page()
          instead.
-
    The first symptom of either of these problems will probably be
    an assertion failure in thread_current(), which checks that
    the `magic' member of the running thread's `struct thread' is
@@ -93,7 +87,9 @@ struct thread
     int prev_priority;
     int64_t waking_time;                /* Time when the thread should be waken up*/
     struct lock *waiting_for_lock;
-    struct list locks_list; 			/* Locks that a thread is holding */
+    struct list locks_list; 		/* Locks that a thread is holding */
+    fixed_p recent_cpu;			/*recent cpu used*/
+    int nice;                           
     struct list_elem allelem;           /* List element for all threads list. */
 
     /* Shared between thread.c and synch.c. */
@@ -107,7 +103,7 @@ struct thread
 
     /* Owned by thread.c. */
     unsigned magic;                     /* Detects stack overflow. */
-  };
+};
 
 /* If false (default), use round-robin scheduler.
    If true, use multi-level feedback queue scheduler.
@@ -152,12 +148,13 @@ bool my_wakingtime_less_func (const struct list_elem *a,const struct list_elem *
 /*Function that compares the priority of threads a and b, returning true if priority a > priority b. This function will be used for scheduling*/
 bool my_pri_greater_func (const struct list_elem *a,const struct list_elem *b,void *aux );
 
-
 /*Function that puts some thread to sleep*/
 void thread_sleep (struct thread *T,int64_t wakingTime);
 
 /*Function that checks if the first elem from sleeping list has waking time less than current time. If so, removes that elem from sleeping list*/
 void thread_waking_time(void);
+
+void thread_priority_donate(struct thread *t, int new_priority);
 
 void sort_rdy_list(void);
 #endif /* threads/thread.h */
